88b0a9de9f4f09c98496552365df2f44fc004501
Michał <michauz2001@gmail.com>
2025-01-25 00:34:43 +0100

    Workout UI

---
+++ WorkoutUi/src/main/java/put/poznan/pl/michalxpz/workoutui/WorkoutUiApplication.java
@@ -0,0 +1,21 @@
+package put.poznan.pl.michalxpz.workoutui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class WorkoutUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutUiApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
+	}
+
+}
---
+++ WorkoutUi/src/test/java/put/poznan/pl/michalxpz/workoutui/WorkoutUiApplicationTests.java
@@ -0,0 +1,13 @@
+package put.poznan.pl.michalxpz.workoutui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
