1ad0e98f5effadc3fe9243f8f0c4f1dae16d7c45
Michał <michauz2001@gmail.com>
2025-01-25 11:04:03 +0100

    docker-compose

---
+++ build-images.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Lista projektów
+projects=("WorkoutIntegration" "WorkoutPlannerRestClient" "WorkoutPlannerRestService" "WorkoutPlannerSOAP" "WorkoutPlannerSoapClient" "WorkoutUI")
+
+# Przejdź przez każdy projekt
+for projectDir in "${projects[@]}"; do
+    echo "Building Docker image for project: $projectDir"
+
+    # Przejdź do katalogu projektu
+    cd "$projectDir" || { echo "Directory $projectDir not found! Skipping..."; continue; }
+
+    # Buduj obraz Dockera za pomocą Maven
+    ./mvnw spring-boot:build-image \
+        -D"spring-boot.build-image.imageName=aznu-workout/${projectDir:l}" \
+        -DskipTests
+
+    # Powrót do katalogu głównego
+    cd ..
+done
